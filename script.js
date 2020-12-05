ac4fa76715a65dd819c39cfa8d2b69b35b74e992
DTrombla <duncan@trombla.org>
2020-12-04 20:26:11 -0800

    added path

--- script.js
+++ script.js
@@ -19,6 +19,10 @@ app.get("/notes", (req, res) => {
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
 
 app.get("/api/notes", (req, res) => {
     res.json(db);
